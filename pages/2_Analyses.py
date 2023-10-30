ba449637db67244830d1f397bdfae5a73040a931
Hugo <109152830+HugoDutel@users.noreply.github.com>
2023-10-30 08:39:37 +0100

    Update 2_Analyses.py

--- pages/2_Analyses.py
+++ pages/2_Analyses.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from fun.fun_static_model import * as fsm
+from fun.fun_static_model import fun_static_model as fsm
 import altair as alt
 
 st.set_page_config(

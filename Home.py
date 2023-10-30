59271a1ce83a14f734a1b7a345e861c28261d4ae
Hugo <109152830+HugoDutel@users.noreply.github.com>
2023-10-30 19:19:23 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -17,7 +17,7 @@ st.markdown('''The objective of this practical is to **discover and understand h
 As a case study, we will focus on two primate species: the mouse lemur (*Microcebus murinus*) and the crab-eating macaque (*Macaca fascicularis*). These two primate species belong to lineages that diverged at least 74 million-years ago during the Cretaceous period, and show conspicuous differences in morphology, ecology and diet.
 
 
-This webapp will allow you to calculate and compare the function of the jaw-closing system in these two species based on measurements made on 3D virtual models available during the practical, as well as data collected from quantitative dissections (see table in the Analysis section).\n You are encouraged to have a look at the maths behind the app (see Maths page) before using it during the practical.
+This webapp will allow you to calculate and compare the function of the jaw-closing system in these two species based on measurements made on 3D virtual models available during the practical, as well as data collected from quantitative dissections (see table in the Analyses page).\n You are encouraged to have a look at the maths behind the app (see Maths page) before using it during the practical.
 
 
 ''')
